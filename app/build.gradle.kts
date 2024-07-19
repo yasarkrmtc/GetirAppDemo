7f04ebf71a67e4fd8bbccf2ac0230187d128bdb3
Yaşar Kiremitci <yasar.krmtc@gmail.com>
2024-07-19 16:15:51 +0300

    build gradle integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
 }
 
 android {
@@ -43,6 +45,8 @@ android {
 
 dependencies {
 
+
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -57,6 +61,32 @@ dependencies {
     //Navigation
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+
+    // Kotlin coroutines and Flow for asynchronous data flow
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx: 2.6.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
+    implementation ("androidx.lifecycle:lifecycle-common-java8:2.6.0")
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+
+    // Retrofit for API requests
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    // OkHttp for logging and network interceptors
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    //Hilt
+    implementation ("com.google.dagger:hilt-android:2.45")
+    kapt ("com.google.dagger:hilt-compiler:2.45")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.google.gms:google-services:4.4.0")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.45")
+        classpath ("com.android.tools.build:gradle:8.0.0")
+
+    }
 }
+
+plugins {
+    id ("com.android.application") version "8.2.2" apply false
+    id ("com.android.library" )version "8.0.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id ("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+    id ("com.google.dagger.hilt.android") version "2.45" apply false
+}
+
+
